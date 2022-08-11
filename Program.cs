24588910ee0301a7411a5969d35b2afd60b91563
unknown <oliver170504@gmail.com>
2022-08-11 14:19:25 +0200

    Attack of the Commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DroneData
 {
@@ -10,6 +11,24 @@ namespace DroneData
     {
         static void Main(string[] args)
         {
+            string Path = @"C:\Users\Oliver N. Jensen\Desktop\04. Manupulation af tekststrenge c#/flyvning1.csv";
+            using (var reader = new StreamReader(Path))
+            {
+                List<string> ListTicks = new List<string>();
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    ListTicks.Add(values[0]);
+                }
+                for (int i = 2; i < ListTicks.Count; i++)
+                {
+                    Console.WriteLine(ListTicks[i]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
