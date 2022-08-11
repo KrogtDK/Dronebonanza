d2b84250d58f61caf7175f554428f9c546e72df2
unknown <oliver170504@gmail.com>
2022-08-11 14:42:31 +0200

    Revenge of the Commit

--- Program.cs
+++ Program.cs
@@ -6,26 +6,28 @@ using System.Threading.Tasks;
 using System.IO;
 
 namespace DroneData
+    // 1. git add .             //
+    // 2. git commit -m "XXX"   //
+    // 3. git push origin master//
 {
     class Program
     {
         static void Main(string[] args)
         {
             string Path = @"C:\Users\Oliver N. Jensen\Desktop\04. Manupulation af tekststrenge c#/flyvning1.csv";
+            // "Lånet" fra Patrick//
+
             using (var reader = new StreamReader(Path))
             {
-                List<string> ListTicks = new List<string>();
-                
+                List<string>[] List = new List<string>[14];
+                    
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(';');
 
-                    ListTicks.Add(values[0]);
-                }
-                for (int i = 2; i < ListTicks.Count; i++)
-                {
-                    Console.WriteLine(ListTicks[i]);
+                    List[1] = values[1];
+
                 }
             }
             Console.ReadKey();
